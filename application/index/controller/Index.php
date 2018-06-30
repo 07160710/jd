5005ee905f13ecc1572a92254a183c55adc4d46c
huangkaifeng <1195745286@qq.com>
2018-06-30 11:19:16 +0800

    前台分类显示部分完成。。

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,6 +7,10 @@ class Index
 {
     public function index()
     {
+        $cate_select = db('cate')->select();
+        $cate_model = model('cate');
+        $cate_list = $cate_model->getChildren($cate_select);
+        dump($cate_list);die;
         return view();
     }
 }
---
+++ application/index/model/Cate.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\index\model;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/6/30
+ * Time: 10:50
+ * 商品分类前台Cate的Model层
+ */
+
+class Cate extends \think\Model
+{
+    public function getChildren($cate_list,$pid=0){
+        $arr = array();
+        foreach ($cate_list as $key=>$value){
+            if($value['cate_pid'] == $pid){
+                $value['children'] = $this->getChildren($cate_list,$value['cate_id']);
+                $arr[] = $value;
+            }
+        }
+        return $arr;
+    }
+}
